a196278bc0169e8439b8e0209310ec8f78952328
Diego Salazar Arp <d.salazararp@gmail.com>
2019-08-30 22:25:35 -0400

    Initial Commit

---
+++ App-Coffee!/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App-Coffee!
+//
+//  Created by Diego Salazar Arp on 30-08-19.
+//  Copyright © 2019 Diego Salazar Arp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
